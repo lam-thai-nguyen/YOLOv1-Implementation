38b443e34b51ee007c13d167bd1c890c08c3765d
Lam Thai Nguyen <thainguyen2893@gmail.com>
2024-07-09 13:31:45 +0700

    update

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 """
-Last edited on: Jul 8, 2024
+Last edited on: Jul 9, 2024
 by: Lam Thai Nguyen
 """
 
@@ -20,22 +20,22 @@ def IoU(box1, box2):
     coord1, coord2 = _box2coord(box1), _box2coord(box2)
     
     # Intersection box
-    x1 = torch.max(coord1[..., 0], coord2[..., 0])
-    y1 = torch.max(coord1[..., 1], coord2[..., 1])
-    x2 = torch.min(coord1[..., 2], coord2[..., 2])
-    y2 = torch.min(coord1[..., 3], coord2[..., 3])
+    x1 = torch.max(coord1[..., 0:1], coord2[..., 0:1])
+    y1 = torch.max(coord1[..., 1:2], coord2[..., 1:2])
+    x2 = torch.min(coord1[..., 2:3], coord2[..., 2:3])
+    y2 = torch.min(coord1[..., 3:4], coord2[..., 3:4])
     
     intersection_area = torch.clamp(x2-x1, min=0) * torch.clamp(y2-y1, min=0)
     
     # Areas
-    area1 = (coord1[..., 2] - coord1[..., 0]) * (coord1[..., 3] - coord1[..., 1])
-    area2 = (coord2[..., 2] - coord2[..., 0]) * (coord2[..., 3] - coord2[..., 1])
+    area1 = (coord1[..., 2:3] - coord1[..., 0:1]) * (coord1[..., 3:4] - coord1[..., 1:2])
+    area2 = (coord2[..., 2:3] - coord2[..., 0:1]) * (coord2[..., 3:4] - coord2[..., 1:2])
     
     # Union area
     union_area = area1 + area2 - intersection_area + 1e-6
     
     # IoU
-    IoU = (intersection_area / union_area).unsqueeze(-1)
+    IoU = intersection_area / union_area
     
     return IoU 
 
@@ -50,12 +50,12 @@ def _box2coord(box):
     Returns:
         coord (tensor) (batch_size, 7, 7, 4)
     """
-    x, y, w, h = box[..., 0], box[..., 1], box[..., 2], box[..., 3]
+    x, y, w, h = box[..., 0:1], box[..., 1:2], box[..., 2:3], box[..., 3:4]
     x1 = x - w / 2
     y1 = y - h / 2
     x2 = x + w / 2
     y2 = y + h / 2
-    coord = torch.stack((x1, y1, x2, y2), dim=-1)
+    coord = torch.cat((x1, y1, x2, y2), dim=-1)
     return coord
 
 
