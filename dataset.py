9772c668ac4c7890295e6aba2dfd8e3d51e9817e
Lam Thai Nguyen <thainguyen2893@gmail.com>
2024-07-13 13:29:34 +0700

    update

--- dataset.py
+++ dataset.py
@@ -6,10 +6,9 @@ by: Lam Thai Nguyen
 import torch
 import os
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import Dataset
 
 
 class VOCDataset(Dataset):
